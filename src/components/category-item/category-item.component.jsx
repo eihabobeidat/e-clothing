5eee435266fb848d1defd184fd1853b4c50c4aae
ehab_obedat <ehab.obedat@network.global>
2023-08-13 20:24:03 +0300

    setup navigation

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,12 +1,16 @@
 // import './category-item.styles.scss'
 
-const Category = ({ title, image }) =>
-<div className="category-container">
-  <div className='background-image' style={{ backgroundImage: `url(${image})` }}></div>
-  <div className="category-body-container">
-    <h2>{title}</h2>
-    <p>Shop Now</p>
+const Category = ({ title, image }) => (
+  <div className="category-container">
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${image})` }}
+    />
+    <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>Shop Now</p>
+    </div>
   </div>
-</div>
+);
 
 export default Category;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,10 +1,14 @@
-import '../../categories.styles.scss';
-import Category from '../category-item/category-item.component';
+import "../../categories.styles.scss";
+import Category from "../category-item/category-item.component";
 
-const Directory = ({categories}) => {
-    return (   <div className="categories-container">
-    {categories.map(({ id, title, imageUrl }) => <Category key={id} title={title} image={imageUrl} />)}
-  </div>);
-}
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map(({ id, title, imageUrl }) => (
+        <Category key={id} title={title} image={imageUrl} />
+      ))}
+    </div>
+  );
+};
 
 export default Directory;
