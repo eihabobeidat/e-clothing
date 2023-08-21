103bd2f45c789aa2f88fd8dcf39c9b3a9bc62777
ehab_obedat <ehab.obedat@network.global>
2023-08-21 21:37:42 +0300

    migrate user context from normal context with useState to context with reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   onAuthStateChangeListener,
   createUserDocumentFromAuth,
@@ -9,8 +9,28 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, { type, paylaod }) => {
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: paylaod };
+    default:
+      throw new Error(`Unhandled type in userReducer: ${type}`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (payload) =>
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload });
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangeListener(async (user) => {
